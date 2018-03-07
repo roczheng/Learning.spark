08a827a966c303fdb9aa1dfd14ad9133744c037d
Peng Zheng <roczheng@126.com>
2018-03-07 22:39:53 +0800

    rddTest

--- project/src/com/spark/rddTest/rddTest.scala
+++ project/src/com/spark/rddTest/rddTest.scala
@@ -5,8 +5,14 @@ object rddTest {
   def main(args: Array[String]) {
     var conf = new SparkConf().setMaster("local").setAppName("testRddMethod")
     val sc = new SparkContext(conf)
-    val arr = sc.parallelize(Array(1,2,3,4,5,6))
-    val result = arr.aggregate(0)(math.max(_, _), _ + _)
-    println(result)
+    //val arr = sc.parallelize(Array(1,2,3,4,5,6),2)
+    val arr = sc.parallelize(Array("abc","b","c","d","e","f"))
+    //val result = arr.aggregate(0)(math.max(_, _), _ + _)
+    //val result = arr.aggregate("")((value, word) => value + word, _+_)
+    //println(result)
+    println(arr)
+    println("---------------")
+    println(arr.cache())
+    arr.foreach(println)
   }
 }
